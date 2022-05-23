ea7d6d29d9883887d0f911b5109ea2b2f3d08a6c
SnigdhadebPaul <91609409+SnigdhadebPaul@users.noreply.github.com>
2022-05-23 23:46:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+quick_draw_data_set=[dog,cat,lion,tiger,giraffe,apple,strawberry,Aircraftcarrier]
+
+function setup(){
+    canvas=createCanvas(300,300);
+   canvas.center();
+   background("white");
+   
+   
+   }
+   function clearCanvas(){
+   background("white");
+   
+   
+   } 
+   
+   
+   
